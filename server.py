c20c45e1628dbf98a9408ad89d507eb5f190168a
Brendan Cordova <cordovalegacy19@gmail.com>
2023-01-05 16:13:17 -0800

    Practice with Sessions

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'supah safe, nothing to see here'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/show')
+def show_user():
+    return render_template('show.html', name_on_template = session['username'], email_on_template = session['email_address'])
+
 @app.route('/users', methods=['POST'])
 def create_users():
     print("Post Information")
     print(request.form)
-    return redirect('/')
+    session['username'] = request.form['name']
+    session['email_address'] = request.form['email']
+    return redirect('/show')
 
 if __name__ == "__main__":
     app.run(debug=True)
